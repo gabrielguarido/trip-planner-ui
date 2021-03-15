b943d47742bb808b329e55c75c38664c59847e18
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-03-15 20:51:04 -0300

    Fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,11 @@ import {SignupComponent} from './pages/signup/signup.component';
 import {PlanComponent} from './pages/plan/plan.component';
 
 export const routes: Routes = [
-  {path: '', component: AppLoginComponent},
   {path: 'login', component: AppLoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'plans', component: PlanComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '**', redirectTo: '/login', pathMatch: 'full'},
 ];
 
 export const AppRoutes: ModuleWithProviders<any> = RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'});
