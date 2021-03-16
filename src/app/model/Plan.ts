554598bd54d08c167938ac0eef5f25f3bafac93f
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-03-16 02:01:09 -0300

    Update Plan

--- src/app/model/Plan.ts
+++ src/app/model/Plan.ts
@@ -6,14 +6,19 @@ export class Plan {
   departureDate: string;
   returnDate: string;
   duration: number;
+  totalExpenses: number;
+  currency: string;
   expenseSet: Expense[];
 
-  constructor(id: number, countryName: string, departureDate: string, returnDate: string, duration: number, expenseSet: Expense[]) {
+  constructor(id: number, countryName: string, departureDate: string, returnDate: string, duration: number,
+              totalExpenses: number, currency: string, expenseSet: Expense[]) {
     this.id = id;
     this.countryName = countryName;
     this.departureDate = departureDate;
     this.returnDate = returnDate;
     this.duration = duration;
+    this.totalExpenses = totalExpenses;
+    this.currency = currency;
     this.expenseSet = expenseSet;
   }
 }
